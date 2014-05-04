2c9cdcf51637d0b3b5e847825453702f187ec2ac
firefish <firefish@firefishpc.(none)>
2014-05-05 00:24:53 +0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,38 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var mesh;
+
+var pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.x = 50;
+pointLight.position.y = 50;
+pointLight.position.z = 50;
+scene.add(pointLight);
+
+var geometry = new THREE.PlaneGeometry( 100, 100 );
+var material = new THREE.MeshLambertMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+var plane = new THREE.Mesh( geometry, material );
+plane.rotation.x = 3.14/2;
+scene.add( plane );
+
+camera.position.z = 60;
+camera.position.y = 40;
+camera.lookAt(new THREE.Vector3( 0, 20, 0 ));
+
+var render = function () {
+    requestAnimationFrame(render, null);
+    mesh.rotation.y += 0.01;
+    renderer.render(scene, camera);
+};
+
+loader = new THREE.JSONLoader();
+loader.load( "teapot.js", function( geometry ) {
+    mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial() );
+    mesh.scale.set( 10, 10, 10 );
+    scene.add( mesh );
+    render();
+} );
